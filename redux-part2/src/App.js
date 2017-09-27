283745f542affbe20c4336b015fffeb81c5f0186
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-09-27 13:28:15 +0300

    connect container

--- redux-part2/src/App.js
+++ redux-part2/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import SearchBar from './containers/SearchBar';
+import WeatherList from './containers/WeatherList';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<SearchBar />
+				<WeatherList />
 			</div>
 		);
 	}
